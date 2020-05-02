d6d85d4f3bed87f26dfa5fd45c9f8d4bb9c7bf53
ashubajpai <ashutosh.bajpai007@gmail.com>
2020-05-02 22:12:50 +0530

    ashu

--- main.tf
+++ main.tf
@@ -29,6 +29,12 @@ resource "random_integer" "app_service_name_suffix" {
   max = 9999
 }
 
+provider "azurerm" {
+  # whilst the `version` attribute is optional, we recommend pinning to a given version of the Provider
+  version = ">=2.0.0"
+  features {}
+}
+
 resource "azurerm_resource_group" "spacegame" {
   name     = "${var.resource_group_name}"
   location = "${var.resource_group_location}"
